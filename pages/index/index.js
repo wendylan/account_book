d7a904dd504a5d256355b2af9eb9a41ebd2f2203
wgh <3063766545@qq.com>
2018-01-25 15:01:07 +0800

    this is a first conmit for wechat applet

---
+++ pages/index/index.js
@@ -0,0 +1,37 @@
+//index.js
+//获取应用实例
+var app = getApp()
+Page({
+    data: {
+        motto: 'Hello World',
+        moviesData : [],
+        userInfo: {}
+    },
+    //事件处理函数
+    bindViewTap: function() {
+        wx.switchTab({
+            url: '../account/account'
+        })
+    },
+    onLoad: function () {
+        console.log('onLoad')
+        var that = this
+        //调用应用实例的方法获取全局数据
+        app.getUserInfo(function(userInfo){
+            //更新数据
+            that.setData({
+                userInfo:userInfo
+            })
+        })
+
+        wx.request({
+            url: 'https://api.douban.com/v2/movie/coming_soon', //仅为示例，并非真实的接口地址
+            header: {
+                'content-type': 'application/json' // 默认值
+            },
+            success: function (res) {
+                console.log(res.data)
+            }
+        })
+    }
+})
---
+++ pages/splash/splash.js
@@ -0,0 +1,13 @@
+Page({
+    data: {
+        imgUrls: [
+            '../../images/1.jpg',
+            '../../images/2.jpg',
+            '../../images/3.jpg'
+        ],
+        indicatorDots: true,
+        autoplay: true,
+        interval: 5000,
+        duration: 1000
+    },
+})
