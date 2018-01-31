48a5a71c7d4fea8902dfe0fc65f8ddbd238303f7
wgh <3063766545@qq.com>
2018-01-31 16:18:14 +0800

    add bill ,my, images and my

--- pages/bill/bill.js
+++ pages/bill/bill.js
@@ -7,11 +7,40 @@ Page({
     data: {
         bill_arr : [],
     },
+    delOne: function(e){
+        let _this = this;
+        wx.showModal({
+            title: '删除',
+            content: '确定删除吗？删除后数据无法恢复',
+            success: function (res) {
+                if (res.confirm) {
+                    console.log('用户点击确定');
+                    let index = e.currentTarget.dataset.index;
+                    _this.data.bill_arr.splice(index, 1);
+                    wx.setStorage({
+                        key: "bill_arr",
+                        data: _this.data.bill_arr
+                    });
+                    _this.showToast();
+                    _this.getStorageData();
+                } else if (res.cancel) {
+                    console.log('用户点击取消')
+                }
+            }
+        });
+    },
 
-    /**
-     * 生命周期函数--监听页面加载
-     */
-    onLoad: function (options) {
+    // 显示删除成功提示
+    showToast() {
+        wx.showToast({
+            title: '删除成功',
+            icon: 'success',
+            duration: 2000
+        });
+    },
+
+    // 获取key为“bill_arr”的数据
+    getStorageData(){
         var _this = this;
         wx.getStorage({
             key: 'bill_arr',
@@ -20,21 +49,27 @@ Page({
                     bill_arr: res.data
                 });
             }
-        })
+        });
+    },
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function (options) {
+        
     },
 
     /**
      * 生命周期函数--监听页面初次渲染完成
      */
     onReady: function () {
-    
+        
     },
 
     /**
      * 生命周期函数--监听页面显示
      */
     onShow: function () {
-    
+        this.getStorageData();
     },
 
     /**
