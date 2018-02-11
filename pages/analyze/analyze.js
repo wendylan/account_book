9b717275a020b3e5ff6a9196ddebff2c855274dc
wgh <3063766545@qq.com>
2018-02-11 11:36:24 +0800

    add chart template

--- pages/analyze/analyze.js
+++ pages/analyze/analyze.js
@@ -9,7 +9,9 @@ Page({
     data: {
         bill_arr: [],
         consumption_chart_arr: [0, 0, 0, 0, 0, 0, 0, 0],
-        earn_chart_arr: [0, 0, 0]
+        earn_chart_arr: [0, 0, 0],
+        consumption_chart_data : [],
+        earn_chart_data : [],
     },
 
     // 获取key为“bill_arr”的数据
@@ -21,112 +23,78 @@ Page({
                 _this.setData({
                     bill_arr: res.data
                 });
+                let temp = _this.changeDataFormat();
+                _this.data.consumption_chart_data = temp.consum;
+                _this.data.earn_chart_data = temp.earn;
+                _this.getChart('consumption-chart', _this.data.consumption_chart_data);
+                _this.getChart('earn-chart', _this.data.earn_chart_data);
             }
         });
     },
-    // 转换数值
-    fetchBillData() {
+
+    // 修改数据格式
+    changeDataFormat(){
+        console.log(this.data.bill_arr);
+        let allData = {};
+        let earnArr = [];
+        let consumArr = [];
         this.data.bill_arr.forEach((item, index) => {
-            if (item.account_type[0] == '水果零食')
-                this.consumption_chart_arr[0] = this.consumption_chart_arr[0] + (+item.sum_value);
-            else if (item.account_type[0] == '餐饮伙食')
-                this.consumption_chart_arr[1] = this.consumption_chart_arr[1] + (+item.sum_value);
-            else if (item.account_type[0] == '出行旅游')
-                this.consumption_chart_arr[2] = this.consumption_chart_arr[2] + (+item.sum_value);
-            else if (item.account_type[0] == '网上购物')
-                this.consumption_chart_arr[3] = this.consumption_chart_arr[3] + (+item.sum_value);
-            else if (item.account_type[0] == '生活日常')
-                this.consumption_chart_arr[4] = this.consumption_chart_arr[4] + (+item.sum_value);
-            else if (item.account_type[0] == '租房水电')
-                this.consumption_chart_arr[5] = this.consumption_chart_arr[5] + (+item.sum_value);
-            else if (item.account_type[0] == '医疗药物')
-                this.consumption_chart_arr[6] = this.consumption_chart_arr[6] + (+item.sum_value);
-            else if (item.account_type[0] == '其它消费')
-                this.consumption_chart_arr[7] = this.consumption_chart_arr[7] + (+item.sum_value);
-            else if (item.account_type[0] == '基本工资')
-                this.earn_chart_arr[0] = this.earn_chart_arr[0] + (+item.sum_value);
-            else if (item.account_type[0] == '公司福利')
-                this.earn_chart_arr[1] = this.earn_chart_arr[1] + (+item.sum_value);
-            else if (item.account_type[0] == '其它入账')
-                this.earn_chart_arr[2] = this.earn_chart_arr[2] + (+item.sum_value);
+            if (item.consumption_or_earn){
+                if (item.account_type -0 == 0 ){
+                    this.data.earn_chart_arr[0] = this.data.earn_chart_arr[0] + (+item.sum_value);
+                }
+                else if (item.account_type -0 == 1){
+                    this.data.earn_chart_arr[1] = this.data.earn_chart_arr[1] + (+item.sum_value);
+                }else{
+                    this.data.earn_chart_arr[2] = this.data.earn_chart_arr[2] + (+item.sum_value);
+                }
+            }else{
+                if (item.account_type -0 == 0){
+                    this.data.consumption_chart_arr[0] = this.data.consumption_chart_arr[0] + (+item.sum_value);
+                }
+                else if (item.account_type -0 == 1){
+                    this.data.consumption_chart_arr[1] = this.data.consumption_chart_arr[1] + (+item.sum_value);
+                }
+                else if (item.account_type -0 == 2) {
+                    this.data.consumption_chart_arr[2] = this.data.consumption_chart_arr[2] + (+item.sum_value);
+                }
+                else if (item.account_type -0 == 3) {
+                    this.data.consumption_chart_arr[3] = this.data.consumption_chart_arr[3] + (+item.sum_value);
+                }
+                else if (item.account_type -0 == 4) {
+                    this.data.consumption_chart_arr[4] = this.data.consumption_chart_arr[4] + (+item.sum_value);
+                }
+                else if (item.account_type -0 == 5) {
+                    this.data.consumption_chart_arr[5] = this.data.consumption_chart_arr[5] + (+item.sum_value);
+                }
+                else if (item.account_type -0 == 6) {
+                    this.data.consumption_chart_arr[6] = this.data.consumption_chart_arr[6] + (+item.sum_value);
+                }
+                else{
+                    this.data.consumption_chart_arr[7] = this.data.consumption_chart_arr[7] + (+item.sum_value);
+                }
+            }
         });
+        let earnText = ["基本工资", "公司福利", "其它入账"];
+        let consumText = ["水果零食", "餐饮伙食", "出行旅游", "网上购物", "生活日常", "租房水电", "医疗药物", "其它消费"];
+        allData.earn = this.dataFormat(earnText, this.data.earn_chart_arr);
+        allData.consum = this.dataFormat(consumText, this.data.consumption_chart_arr);
+        return allData;
     },
 
-    changeData(){
-        var consumption_data = {
-            labels: [
-                "水果零食",
-                "餐饮伙食",
-                "出行旅游",
-                "网上购物",
-                "生活日常",
-                "租房水电",
-                "医疗药物",
-                "其它消费",
-            ],
-            datasets: [{
-                data: this.consumption_chart_arr,
-                backgroundColor: [
-                    "#999933",
-                    "#FF9933",
-                    "#FF6666",
-                    "#36A2EB",
-                    "#666699",
-                    "#CCFF00",
-                    "#66CCCC",
-                    "#663366"
-                ]
-            }]
-        };
-        var earn_data = {
-            labels: [
-                "基本工资",
-                "公司福利",
-                "其它入账"
-            ],
-            datasets: [{
-                data: this.earn_chart_arr,
-                backgroundColor: [
-                    "#13CE66",
-                    "#F7BA2A",
-                    "#FF4949"
-                ]
-            }]
-        };
-        // var consumption_ctx = document.getElementById("consumption-chart").getContext("2d");
-        // var consumption_ctx = wx.createCanvasContext('consumption-chart');
-        // var earn_ctx = document.getElementById("earn-chart").getContext("2d");
-        // var earn_ctx = wx.createCanvasContext('earn-chart');
-        new Chart('consumption-chart', {
-            type: 'pie',
-            data: consumption_data
-        });
-        new Chart('earn-chart', {
-            type: 'pie',
-            data: earn_data
-        });
+    // 数据格式转换
+    dataFormat(arr, dataArr){
+        let data = [];
+        for(let i = 0; i<arr.length; i++){
+            data.push({ 'name' : arr[i], 'data' : dataArr[i] });
+        }
+        return data;
     },
-
-    getChart(str){
+    getChart(str, data){
         new wxCharts({
             canvasId: str,
             type: 'pie',
-            series: [{
-                name: 'cat1',
-                data: 50,
-            }, {
-                name: 'cat2',
-                data: 30,
-            }, {
-                name: 'cat3',
-                data: 1,
-            }, {
-                name: 'cat4',
-                data: 1,
-            }, {
-                name: 'cat5',
-                data: 46,
-            }],
+            series: data,
             width: 350,
             height: 220,
             dataLabel: true
@@ -136,7 +104,7 @@ Page({
      * 生命周期函数--监听页面加载
      */
     onLoad: function (options) {
-
+        
     },
 
     /**
@@ -151,8 +119,6 @@ Page({
      */
     onShow: function () {
         this.getStorageData();
-        this.getChart('consumption-chart');
-        this.getChart('earn-chart');
     },
 
     /**
